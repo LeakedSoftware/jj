af07d143a4281ed785295822ba256b9f7d2348c2
LeakedSoftware <113989040+LeakedSoftware@users.noreply.github.com>
2024-11-01 11:23:40 +0200

    Add files via upload

--- test dlr hikka.py
+++ test dlr hikka.py
@@ -1,6 +1,6 @@
-from hikka import loader, utils
-from telethon import events
 import os
+from .. import loader, utils
+from asyncio import sleep
 
 def register(cb):
     cb(ReplyDownloaderMod1())
@@ -13,50 +13,59 @@ class ReplyDownloaderMod1(loader.Module):
         """Команда .dlr <реплай на файл> <название (по желанию)> скачивает файл, либо сохраняет текст в файл на который был сделан реплай, и отправляет в избранное."""
         name = utils.get_args_raw(message)
         reply = await message.get_reply_message()
-
         if reply:
             sent_anything = False  # Флаг для отслеживания отправки содержимого
 
-            # Сохраняем текст, если он есть
+            # Отправка текста, если он присутствует в ответе
             if reply.text:
-                text_fname = f'{name or message.id + reply.id}_text.txt'
+                text = reply.text
+                text_fname = f'{name or message.id+reply.id}_text.txt'
                 with open(text_fname, 'w') as text_file:
-                    text_file.write(reply.text)
+                    text_file.write(text)
+                
+                # Отправляем файл с текстом в "Избранное"
                 await message.client.send_file('me', text_fname)
-                os.remove(text_fname)
+                os.remove(text_fname)  # Удаляем текстовый файл после отправки
                 sent_anything = True
 
-            # Проверяем, есть ли медиафайлы в реплае
-            if reply.media:
-                media_files = []
-
-                # Если это альбом, обрабатываем его
-                if hasattr(reply.media, 'media_album_id'):
-                    # Получаем все фотографии из альбома
-                    media_files = await message.client.get_messages(reply.chat_id, filter=events.filters.InputMessages.FilterPhotos, limit=None, max_id=reply.id)
-
-                else:
-                    media_files.append(reply)  # Просто одно сообщение с медиа
-
-                # Скачиваем и отправляем каждый файл
-                for index, media_message in enumerate(media_files):
-                    if media_message.media:
-                        fname = f'{name or message.id + reply.id}_{index}.file'
-                        await message.client.download_media(media_message, fname)
-                        await message.client.send_file('me', fname)
-                        os.remove(fname)
-                        sent_anything = True
-
-            # Сообщение, если отправлено содержимое
+            # Отправка файла, если он присутствует в ответе
+            if reply.file:
+                ext = reply.file.ext or ""  # Получаем расширение, если оно есть
+                file_fname = f'{name or message.id+reply.id}{ext}'
+                await message.client.download_media(reply, file_fname)
+                
+                # Отправляем скачанный файл в "Избранное"
+                await message.client.send_file('me', file_fname)
+                os.remove(file_fname)  # Удаляем скачанный файл после отправки
+                sent_anything = True
+            
             if sent_anything:
                 await message.edit('Содержимое успешно отправлено в Избранное.')
             else:
                 await message.edit('Нет текста или файлов для отправки.')
         else:
-            await message.edit('Нет реплая.')
+            return await message.edit('Нет реплая.')
 
-    @events.register(events.Album)
-    async def albumHandler(self, event):
-        """Обработчик альбома."""
-        # Пересылаем весь альбом в нужный чат
-        await event.forward_to('me')
+    async def ulfcmd(self, message):
+        """Команда .ulf <d>* <название файла> отправляет файл в чат.\n* - удалить файл после отправки."""
+        name = utils.get_args_raw(message)
+        d = False
+        if('d ' in name):
+            d = True
+        if name:
+            try:
+                name = name.replace('d ', '')
+                await message.edit(f'Отправляем <code>{name}</code>...')
+                if d:
+                    await message.client.send_file(message.to_id, f'{name}')
+                    await message.edit(f'Отправляем <code>{name}</code>... Успешно!\nУдаляем <code>{name}</code>...')
+                    os.remove(name)
+                    await message.edit(f'Отправляем <code>{name}</code>... Успешно!\nУдаляем <code>{name}</code>... Успешно!')
+                    await sleep(0.5)
+                else:
+                    await message.client.send_file(message.to_id, name)
+            except:
+                return await message.edit('Такой файл не существует.')
+            await message.delete()
+        else:
+            return await message.edit('Нет аргументов.')
