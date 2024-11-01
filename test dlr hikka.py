142d48171354ea13fd18c6085881bb833a443fb8
LeakedSoftware <113989040+LeakedSoftware@users.noreply.github.com>
2024-11-01 10:27:17 +0200

    Add files via upload

--- test dlr hikka.py
+++ test dlr hikka.py
@@ -14,6 +14,8 @@ class ReplyDownloaderMod1(loader.Module):
         name = utils.get_args_raw(message)
         reply = await message.get_reply_message()
         if reply:
+            sent_anything = False  # Флаг для отслеживания отправки содержимого
+
             # Отправка текста, если он присутствует в ответе
             if reply.text:
                 text = reply.text
@@ -24,18 +26,23 @@ class ReplyDownloaderMod1(loader.Module):
                 # Отправляем файл с текстом в "Избранное"
                 await message.client.send_file('me', text_fname)
                 os.remove(text_fname)  # Удаляем текстовый файл после отправки
+                sent_anything = True
 
             # Отправка файла, если он присутствует в ответе
             if reply.file:
-                ext = reply.file.ext
+                ext = reply.file.ext or ""  # Получаем расширение, если оно есть
                 file_fname = f'{name or message.id+reply.id}{ext}'
                 await message.client.download_media(reply, file_fname)
                 
                 # Отправляем скачанный файл в "Избранное"
                 await message.client.send_file('me', file_fname)
                 os.remove(file_fname)  # Удаляем скачанный файл после отправки
+                sent_anything = True
             
-            await message.edit('Содержимое успешно отправлено в Избранное.')
+            if sent_anything:
+                await message.edit('Содержимое успешно отправлено в Избранное.')
+            else:
+                await message.edit('Нет текста или файлов для отправки.')
         else:
             return await message.edit('Нет реплая.')
 
@@ -49,7 +56,7 @@ class ReplyDownloaderMod1(loader.Module):
             try:
                 name = name.replace('d ', '')
                 await message.edit(f'Отправляем <code>{name}</code>...')
-                if d == True:
+                if d:
                     await message.client.send_file(message.to_id, f'{name}')
                     await message.edit(f'Отправляем <code>{name}</code>... Успешно!\nУдаляем <code>{name}</code>...')
                     os.remove(name)
